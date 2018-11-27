36b8be1108e784e335d615eae5034ff8f6a82295
Ludovic <ludovic.giezendanner@epfl.ch>
2018-11-27 18:10:01 +0100

    add fragment to mainActivity

--- mobile/src/main/java/com/example/d_wen/freerun/MainActivity.java
+++ mobile/src/main/java/com/example/d_wen/freerun/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.d_wen.freerun;
 
+import android.net.Uri;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyProfileFragment.
+        OnFragmentInteractionListener, RunPreparationFragment.OnFragmentInteractionListener{
+    private SectionsStatePagerAdapter mSectionStatePagerAdapter;
+    private MyProfileFragment myProfileFragment;
+    private RunPreparationFragment runPreparationFragment;
+
     // TODO: faire une app cool
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSectionStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        myProfileFragment = new MyProfileFragment();
+        runPreparationFragment = new RunPreparationFragment();
+
+        ViewPager mViewPager = findViewById(R.id.mainViewPager);
+        setUpViewPager(mViewPager);
+
+        // Set NewRecordingFragment as default tab once started the activity
+        mViewPager.setCurrentItem(mSectionStatePagerAdapter
+                .getPositionByTitle("New Recording"));
+    }
+
+    private void setUpViewPager(ViewPager mViewPager) {
+        mSectionStatePagerAdapter.addFragment(runPreparationFragment, "RUN");
+        mSectionStatePagerAdapter.addFragment(myProfileFragment, "PROFILE");
+        mViewPager.setAdapter(mSectionStatePagerAdapter);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ mobile/src/main/java/com/example/d_wen/freerun/SectionsStatePagerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.d_wen.freerun;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    public int getPositionByTitle(String title) {
+        return mFragmentTitleList.indexOf(title);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
