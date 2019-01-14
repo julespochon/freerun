feecb88e4c81e369bb84440c35736985474202ac
danielwenger <42643690+danielwenger@users.noreply.github.com>
2019-01-14 18:54:42 +0100

    history

--- mobile/src/main/java/com/example/d_wen/freerun/HistoryFragment.java
+++ mobile/src/main/java/com/example/d_wen/freerun/HistoryFragment.java
@@ -3,75 +3,159 @@ package com.example.d_wen.freerun;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HistoryFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HistoryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1="param1";
-    private static final String ARG_PARAM2="param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final String TAG = this.getClass().getSimpleName();
+
 
     private OnFragmentInteractionListener mListener;
+
+    private ListView listView;
+    private View fragmentView;
+    private RecordingAdapter adapter;
+    private MyFirebaseRecordingListener mFirebaseRecordingListener;
+    private DatabaseReference databaseRef;
+    private String idUser;
+
 
     public HistoryFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryFragment newInstance(String param1, String param2) {
-        HistoryFragment fragment=new HistoryFragment();
-        Bundle args=new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1=getArguments().getString(ARG_PARAM1);
-            mParam2=getArguments().getString(ARG_PARAM2);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        // Inflate the layout for this fragment
+        fragmentView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        listView = fragmentView.findViewById(R.id.myHistoryList);
+        adapter = new RecordingAdapter(getActivity(), R.layout.row_history);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), "Exercise: " + ((TextView) view.findViewById(R.id
+                        .exerciseType)).getText().toString() + " on " + ((TextView) view
+                        .findViewById(R.id.exerciseDateTime)).getText().toString(), Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+
+        idUser = getActivity().getIntent().getExtras().getString(MyProfileFragment.USER_ID);
+
+        return fragmentView;
+    }
+
+    private class RecordingAdapter extends ArrayAdapter<Recording> {
+        private int row_layout;
+
+        RecordingAdapter(FragmentActivity activity, int row_layout) {
+            super(activity, row_layout);
+            this.row_layout = row_layout;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            //Reference to the row View
+            View row = convertView;
+
+            if (row == null) {
+                //Inflate it from layout
+                row = LayoutInflater.from(getContext()).inflate(row_layout, parent, false);
+            }
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale
+                    .getDefault());
+            ((TextView) row.findViewById(R.id.exerciseType)).setText(getItem(position)
+                    .exerciseType);
+            ((TextView) row.findViewById(R.id.exerciseDateTime)).setText(formatter.format(new
+                    Date(getItem(position).exerciseDateTime)));
+            ((TextView) row.findViewById(R.id.exerciseDevice)).setText(getItem(position)
+                    .exerciseSmartWatch ? "yes" : "no");
+            ((TextView) row.findViewById(R.id.exerciseDevice2)).setText(getItem(position)
+                    .exerciseHRbelt ? "yes" : "no");
+
+            return row;
+        }
+    }
+
+    public class Recording {
+        protected String exerciseType;
+        protected long exerciseDateTime;
+        protected boolean exerciseSmartWatch;
+        protected boolean exerciseHRbelt;
+    }
+
+    private class MyFirebaseRecordingListener implements ValueEventListener {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            adapter.clear();
+            for (final DataSnapshot rec : dataSnapshot.getChildren()) {
+                final Recording recording = new Recording();
+                recording.exerciseType = rec.child("exercise_type").getValue().toString();
+                recording.exerciseDateTime = Long.parseLong(rec.child("datetime").getValue()
+                        .toString());
+                recording.exerciseSmartWatch = Boolean.parseBoolean(rec.child("switch_watch")
+                        .getValue().toString());
+                recording.exerciseHRbelt = Boolean.parseBoolean(rec.child("switch_hr_belt")
+                        .getValue().toString());
+                adapter.add(recording);
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.v(TAG, databaseError.toString());
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+    public void onResume() {
+        super.onResume();
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+        mFirebaseRecordingListener = new MyFirebaseRecordingListener();
+        databaseRef.child("profiles").child(idUser).child("recordings").addValueEventListener
+                (mFirebaseRecordingListener);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        databaseRef.child("profiles").child(idUser).child("recordings").removeEventListener
+                (mFirebaseRecordingListener);
     }
 
     @Override
@@ -91,16 +175,6 @@ public class HistoryFragment extends Fragment {
         mListener=null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
